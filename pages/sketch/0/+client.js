c39d65543fc3a666a66b83a51a894299364f857b
diselostudio <diselo.studio@proton.me>
2024-03-02 19:17:32 +0000

    fix: sketch 000 new layout

--- pages/sketch/0/+client.js
+++ pages/sketch/0/+client.js
@@ -5,11 +5,11 @@ import "#root/analytics.client";
 (function () {
     const canvas = document.getElementById("experience");
     const sandbox = new GlslCanvas(canvas);
-    canvas.style.width = `${window.innerWidth / 2}px`
+    canvas.style.width = `${window.innerWidth - 40}px`
     sandbox.load(shader);
 
     window.addEventListener('resize', function () {
-        canvas.style.width = `${window.innerWidth / 2}px`
+        canvas.style.width = `${window.innerWidth - 40}px`
     })
 
 })();
